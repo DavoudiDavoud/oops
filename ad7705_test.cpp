8577b779671b7a98282ac57a71f8485eef3abb75
DavoudiDavoud <davoudi.davoud@yahoo.com>
2016-04-05 15:34:20 +0100

    Update ad7705_test.cpp

--- ad7705_test.cpp
+++ ad7705_test.cpp
@@ -172,12 +172,12 @@ int main(int argc, char *argv[])
 	writeReset(fd);
 
 	// tell the AD7705 that the next write will be to the clock register
-	writeReg(fd,0x20);
+	writeReg(fd,0x21);
 	// write 00001100 : CLOCKDIV=1,CLK=1,expects 4.9152MHz input clock
 	writeReg(fd,0x0C);
 
 	// tell the AD7705 that the next write will be the setup register
-	writeReg(fd,0x10);
+	writeReg(fd,0x11);
 	// intiates a self calibration and then after that starts converting
 	writeReg(fd,0x40);
 
@@ -192,7 +192,7 @@ int main(int argc, char *argv[])
 	  }
 
 	  // tell the AD7705 to read the data register (16 bits)
-	  writeReg(fd,0x38);
+	  writeReg(fd,0x39);
 	  // read the data register by performing two 8 bit reads
 	  int value = readData(fd)-0x8000;
 		fprintf(stderr,"data = %d       \r",value);
